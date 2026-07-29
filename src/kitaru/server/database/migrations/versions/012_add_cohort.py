42f65395edad8de3483e04458913af55ad17838d
Michael Schuster <michael.schuster.ffb@googlemail.com>
2026-07-29 20:57:31 +0200

    Chain cohort migration after experiment migration

--- src/kitaru/server/database/migrations/versions/012_add_cohort.py
+++ src/kitaru/server/database/migrations/versions/012_add_cohort.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "012_add_cohort"
-down_revision = "010_add_session"
+down_revision = "011_add_experiment"
 branch_labels = None
 depends_on = None
 
