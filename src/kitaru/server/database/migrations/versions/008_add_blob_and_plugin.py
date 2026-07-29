d0a708bad023cbabfc844462d9bc05a54e6fd5ce
Michael Schuster <michael.schuster.ffb@googlemail.com>
2026-07-29 19:42:08 +0200

    Chain blob and plugin migration after tag and worker migration

--- src/kitaru/server/database/migrations/versions/008_add_blob_and_plugin.py
+++ src/kitaru/server/database/migrations/versions/008_add_blob_and_plugin.py
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "008_add_blob_and_plugin"
-down_revision = "006_add_device"
+down_revision = "009_add_tag_and_worker"
 branch_labels = None
 depends_on = None
 
